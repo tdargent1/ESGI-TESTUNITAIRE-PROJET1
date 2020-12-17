2f52b123d5889bcc8de516cc613e7e755e8b8f3b
LudovicCollignon <lud.collignon@gmail.com>
2020-12-17 18:17:28 +0100

    ajout MailService.php

---
+++ app/src/Service/MailService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Item;
+use App\Entity\User;
+use App\Entity\ToDoList;
+use App\Repository\ToDoListRepository;
+
+class MailService
+{
+    public function envoieMail() 
+    {
+        //TODO
+    }
+}
